0799a689fd7c0fec21a04e1619afa75f3a7ba1cc
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2020-11-16 14:18:15 -0300

    Changed gitignore

---
+++ build/precache-manifest.aeb8c40a2823a1836294eadc5a5db1e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f9c4716b00ab6f155dd1b0dd0b7428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66157fde7623569efe2f",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "2dc2c0986a09bde6820c",
+    "url": "/static/css/main.607d1142.chunk.css"
+  },
+  {
+    "revision": "66157fde7623569efe2f",
+    "url": "/static/js/2.65ddf710.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.65ddf710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc2c0986a09bde6820c",
+    "url": "/static/js/main.46c3b9a5.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
